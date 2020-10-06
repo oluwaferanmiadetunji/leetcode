773bb1de049560713e934b2b7737db6c7d4e9301
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-10-06 23:12:05 +0100

    .

---
+++ 1.js
@@ -0,0 +1,34 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+	// create a map object
+	let numberIndex = new Map();
+	// initialize the result array
+	let result = [];
+
+	for (let i = 0; i < nums.length; i++) {
+		// current number
+		let num = nums[i];
+		// difference
+		let difference = target - num;
+
+		// check if diiference is in map
+		if (numberIndex.has(difference)) {
+			result[0] = numberIndex.get(difference);
+			result[1] = i;
+
+			// return result
+			return result;
+		}
+		numberIndex.set(num, i);
+	}
+
+	return result;
+};
+
+twoSum([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 30);
